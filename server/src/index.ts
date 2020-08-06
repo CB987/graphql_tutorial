64733156c76ea2b7e5c81aa8187bf244d6107dfb
clare <grimalkinclare@gmail.com>
2020-08-06 16:06:56 -0400

    graphql tutorial

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const port = 9000;
+
+const one: number = 1;
+const two: number = 2;
+
+app.get('/', (req, res) => res.send(`whatup bitches 1 + 2 = ${one + two}`));
+
+app.listen(port);
+
+console.log(`[app]: https://localhost:${port}`);
